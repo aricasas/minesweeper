5ec6d1939616341bb7102db90eb5e525c658884b
Ari <aricasasn@gmail.com>
2020-10-01 20:53:47 -0500

    Added clippy lints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#![warn(
+    clippy::all,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::correctness,
+    clippy::complexity,
+    clippy::perf
+)]
+
 fn main() {
     println!("Hello, world!");
 }
